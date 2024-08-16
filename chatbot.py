9a8bc101e89953cdda1b8055bde90a8789e353ff
safwaahmad <sahmedsaffi@gmail.com>
2024-08-16 11:51:31 +0500

    add

--- chatbot.py
+++ chatbot.py
@@ -2,26 +2,17 @@ import streamlit as st
 from langchain.llms import HuggingFaceHub
 import os
 
-# # Replace with your actual Hugging Face token
-# hf_token = os.getenv('HF_Token')
+# Replace with your actual Hugging Face token
+hf_token = os.getenv('HF_Token')
 
-# # Initialize the HuggingFace model
-# repo_id = "gpt2"  # Make sure this is the correct repository ID
-# llm = HuggingFaceHub(repo_id=repo_id, huggingfacehub_api_token=HF_Token, model_kwargs={"max_length": 128, "temperature": 0.7})
-# llm = HuggingFaceHub(repo_id="gpt2", 
-#                        huggingfacehub_api_token="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", 
-#                        model_kwargs={"max_length": 128, "temperature": 0.7})
+# Initialize the HuggingFace model
 repo_id = "gpt2"  # Make sure this is the correct repository ID
+llms = HuggingFaceHub(repo_id=repo_id, huggingfacehub_api_token=HF_Token, model_kwargs={"max_length": 128, "temperature": 0.7})
 
-# Initialize the HuggingFaceHub object
-llms = HuggingFaceHub(
-    repo_id=repo_id,
-    huggingfacehub_api_token=HF_Token,
-    model_kwargs={"max_length": 128, "temperature": 0.7})
 # Streamlit interface
 st.title("Chat with GPT-2")
 st.write("This chatbot is powered by a GPT-2 model hosted on Hugging Face.")
-st.write("Developed by Safwan Ahmad Saffi")
+
 user_input = st.text_input("You:", "Type your message here...")
 
 if st.button("Send"):
